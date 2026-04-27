15a842943d3cba26b593fde8b6a00cc589248db1
mohammadgoodarzi956-commits <mohammad.goodarzi956@gmail.com>
2026-04-28 01:42:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+async function signUp() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  await supabaseClient.auth.signUp({
+    email,
+    password
+  });
+
+  alert("Check email for confirmation");
+}
+
+async function signIn() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  let { data, error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (data.user) {
+    document.getElementById("app").style.display = "block";
+    loadTasks();
+  }
+}
+
+async function addTask() {
+  let task = document.getElementById("task").value;
+
+  let user = (await supabaseClient.auth.getUser()).data.user;
+
+  await supabaseClient.from("tasks").insert([
+    {
+      user_id: user.id,
+      subject: task,
+      status: "pending"
+    }
+  ]);
+
+  loadTasks();
+}
+
+async function loadTasks() {
+  let user = (await supabaseClient.auth.getUser()).data.user;
+
+  let { data } = await supabaseClient
+    .from("tasks")
+    .select("*")
+    .eq("user_id", user.id);
+
+  let html = "";
+
+  data.forEach(t => {
+    html += `<p>${t.subject}</p>`;
+  });
+
+  document.getElementById("tasks").innerHTML = html;
+}
---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://rqcfklrlhvxuyexddurw.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_trJkYhJNU4Z7g7x4WY-EgA_dlfrqi2F";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
